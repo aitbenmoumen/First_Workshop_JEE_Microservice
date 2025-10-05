81de48379d0965d3866ef2fe82d045c4f2edba85
aitbenmoumen <a.aitbenmoumen@gmail.com>
2025-10-05 16:24:52 +0100

    Rest

--- src/main/java/com/aitbenmoumen/first_microservice/repositories/BankAccountRepository.java
+++ src/main/java/com/aitbenmoumen/first_microservice/repositories/BankAccountRepository.java
@@ -2,6 +2,8 @@ package com.aitbenmoumen.first_microservice.repositories;
 
 import com.aitbenmoumen.first_microservice.entities.BankAccount;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource
 public interface BankAccountRepository extends JpaRepository<BankAccount, String> {
 }
